21322e5734a99148bdbbb8ba4a140b1fe6d00c18
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-17 18:00:51 +0000

    Automated update

--- src/Soenneker.Notion.OpenApiClient/Models/PartialUserObjectResponse.cs
+++ src/Soenneker.Notion.OpenApiClient/Models/PartialUserObjectResponse.cs
@@ -7,10 +7,11 @@ using System.IO;
 using System;
 namespace Soenneker.Notion.OpenApiClient.Models
 {
+    /// <summary>
+    /// The user who created the data source.
+    /// </summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-    #pragma warning disable CS1591
     public partial class PartialUserObjectResponse : IParsable
-    #pragma warning restore CS1591
     {
         /// <summary>The id property</summary>
         public Guid? Id { get; set; }
